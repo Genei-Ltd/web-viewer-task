102a489841a3a7106e77df2204923b56f56c6ee7
Adrien Wald <adrien@genei.io>
2022-07-22 08:26:44 +0100

    setup document and types

---
+++ src/document.ts
@@ -0,0 +1,4 @@
+import documentUntyped from './document.json'
+import { WebDocument } from './types'
+
+export const webDocument = documentUntyped as WebDocument
---
+++ src/types.ts
@@ -0,0 +1,37 @@
+export type ChildParent = {
+  tag?: string | null
+}
+
+export type ChildNode = {
+  type: 'child'
+  key: number
+  val: string
+  parent: ChildParent
+}
+
+export type ParentNode = {
+  type: 'parent'
+  key: number
+  tag: string
+  attrs?: Record<string, unknown> | null
+  children: (ParentNode | ChildNode)[]
+}
+
+export type DocumentNode = ParentNode | ChildNode
+
+export type WebDocument = ParentNode
+
+export type WebDocumentPoint = {
+  key: number
+  offset: number
+}
+
+export type WebDocumentRange = {
+  start: WebDocumentPoint
+  end: WebDocumentPoint
+}
+
+export type WebDocumentSelection = {
+  anchor: WebDocumentPoint
+  focus: WebDocumentPoint
+}
